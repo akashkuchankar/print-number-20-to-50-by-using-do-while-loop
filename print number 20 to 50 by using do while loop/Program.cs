71af1ee6d2c54622d1cfeaaea1e8f983ceb3c258
akashkuchankar <kuchankarakash@gmail.com>
2022-10-04 06:58:58 -0700

    Add project files.

---
+++ print number 20 to 50 by using do while loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_number_20_to_50_by_using_do_while_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 20;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i <= 50);
+
+        }
+    }
+}
